936e89afa34df114400aa863287754d0ec8d26f7
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-10 19:36:04 -0400

    Fixed typos, added more project files to .gitignore

--- backend/app.ts
+++ backend/app.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import migration routes from './routes/migrationRoutes';
+import migrationRoutes from './routes/migrationRoutes';
 
 
 dotenv.config();
@@ -15,14 +15,14 @@ app.use(cors());
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URI as string {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI as string, {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected'))
-    .catch((error) => console.log('Error connecting to MongoDB:' error));
+    .catch((error) => console.log('Error connecting to MongoDB:', error));
 
 
 app.use('/api/migrate', migrationRoutes);
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server running on port &{PORT}'))
+app.listen(PORT, () => console.log('Server running on port &{PORT}'));
 
