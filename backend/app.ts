ea5533f05bf0e9e69d825fb08636f98ca510e40d
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-10 19:32:53 -0400

    Added logic for backend application server

---
+++ backend/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import migration routes from './routes/migrationRoutes';
+
+
+dotenv.config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI as string {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch((error) => console.log('Error connecting to MongoDB:' error));
+
+
+app.use('/api/migrate', migrationRoutes);
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => console.log('Server running on port &{PORT}'))
+
