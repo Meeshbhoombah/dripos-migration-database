6daaf1206b59bd0a432082050e915eac95575205
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-10 19:46:57 -0400

    Added Customer Model with MongoDB

---
+++ backend/models/customerModel.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+
+interface ICustomer extends Document {
+    name: string;
+    email: string;
+    stripeId: string;
+    transactions: string[];
+}
+
+
+const CustomerSchema: Schema = newSchema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    stripeId: { type: String, required: true }
+    transactions: [{type: String}],
+});
+
+
+export default mongoose.model<ICustomer>('Customer', CustomerSchema);
+
+
