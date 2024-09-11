5963f270d09ff4332ab53cf17f3d19b9451c99e5
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-11 19:54:58 -0400

    Fixed return statement and switch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Migration from './pages/Migration';
 
 import logo from './logo.svg';
@@ -8,15 +8,15 @@ import './App.css';
 
 
 const App: React.FC = () => {
-    return {
+    return (
         <Router>
             <div className="App">
-                <Switch>
-                    <Router exact path="/" component={Migration} />
-                </Switch>
+                <Routes>
+                    <Route exact path="/" component={Migration} />
+                </Routes>
             </div>
         </Router>
-    };
+    );
 };
 
 
