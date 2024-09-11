c1ac7daeef5223f5ea84da8efb6cd0888de516d1
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-10 20:13:30 -0400

    Added Stripe Integration

---
+++ backend/services/stripeService.ts
@@ -0,0 +1,17 @@
+import Stripe from 'stripe';
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {apiCersion: '2020-08-27'});
+
+
+export const fetchCustomerPayments = async (stripeId: string) => {
+    try {
+        const payments = await stripe.pamentIntents.list({ customer: stripeId });
+        return payments.data;
+    } catch (error) {
+        console.error('Error fetching payments from Stripe:', error);
+        throw error;
+    }
+};
+
+
