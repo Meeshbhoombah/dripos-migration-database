e2787f1c52b8d385a4ae00c708a07c174b70848b
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-11 20:26:51 -0400

    Fixed Migration to be the actual component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
         <Router>
             <div className="App">
                 <Routes>
-                    <Route exact path="/" component={Migration} />
+                    <Route path="/" element={<Migration />} />
                 </Routes>
             </div>
         </Router>
