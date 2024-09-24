ec4a2f4b3e8c01b7bf77fb047145ec7b55094548
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-24 14:40:46 -0400

    Fixed API versioning

--- backend/services/stripeService.ts
+++ backend/services/stripeService.ts
@@ -1,12 +1,13 @@
 import Stripe from 'stripe';
 
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {apiVersion: '2020-08-27'});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {apiVersion: '2024-06-20'});
 
 
 export const fetchCustomerPayments = async (stripeId: string) => {
     try {
         const payments = await stripe.paymentIntents.list({ customer: stripeId });
+
         return payments.data;
     } catch (error) {
         console.error('Error fetching payments from Stripe:', error);
