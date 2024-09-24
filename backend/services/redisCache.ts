cc34594fdb1b38e2ec321d7dae249163d517fdb8
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-09-24 15:13:53 -0400

    Updated setCache function with an expiration date

--- backend/services/redisCache.ts
+++ backend/services/redisCache.ts
@@ -8,22 +8,18 @@ client.on('error', (err) => {
 });
 
 
-export const setCache = (key: string, value: any): Promise<void> => {
-    return new Promise((resolve, reject) => {
-        client.set(key, value, 'EX', 3600, (err: any) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve();
-            }
-        });
+export const setCache = (key: string, value: any) => {
+    // set with an expiration time of 1 hour (translated to seconds)
+    // https://stackoverflow.com/questions/15861424/node-redis-set-with-ex-and-nx
+    client.set(key, value, {
+        EX: 3600 
     });
 }
 
 
 export const getCache = (key: string): Promise<any> => {
     return new Promise((resolve, reject) => {
-        client.get(key, (err: any, data: any) => {
+        client.get(key, (err, data) => {
             if (err) reject(err);
             resolve(JSON.parse(data as string));
         });
