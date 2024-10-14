8dfd9a0418dd428b5528043b9baa80c7224f2be3
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-14 08:19:16 -0400

    Added cases handling Stripe webhook events

--- backend/controllers/webhookController.ts
+++ backend/controllers/webhookController.ts
@@ -7,6 +7,7 @@ import {
 
     addSource,
     updateSource,
+    deleteSource,
 
     addPayment
 } from 'repositories/customer';
@@ -19,6 +20,27 @@ export function handle(req: Request, res: Response) {
     createMigration(Event);
 
     switch (event.type) {
+        // Because `dripos-migration-database` stores data permanetly, we do 
+        // not need to respond to deletion events other than to demarcate that
+        // said event deletes some data (e.g: source deletion events)
+        case 'customer.created': {
+        
+        },
+        case 'customer.updated': {
+        
+        },
+        case 'customer.source.created': {
+        
+        },
+        case 'customer.source.updated': {
+        
+        },
+        case 'customer.source.deleted': {
+        
+        },
+        case 'payment_intent.succeeded': {
+
+        }
         default: {
             console.log('Unlandled event type: ', event) 
         } 
