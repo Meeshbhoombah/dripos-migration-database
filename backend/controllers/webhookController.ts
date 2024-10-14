e12a54dfdb683311d8681ea8dbada32d5044d3de
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-14 08:12:49 -0400

    Added pseudocode for import of customer repository functions

---
+++ backend/controllers/webhookController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+// import { createMigration } from 'repositories/migration';
+/*
+import { 
+    createCustomer, 
+    updateCustomer,
+
+    addSource,
+    updateSource,
+
+    addPayment
+} from 'repositories/customer';
+*/
+
+export function handle(req: Request, res: Response) {
+    /*
+    let event = constructEvent(req.body);
+
+    createMigration(Event);
+
+    switch (event.type) {
+        default: {
+            console.log('Unlandled event type: ', event) 
+        } 
+    }
+    */
+}
+
