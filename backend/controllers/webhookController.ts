acc5ec6df089160ae654fac7f8523dd4c2b77c42
Meeshbhoombah <rohanmishra2017@gmail.com>
2024-10-14 08:54:20 -0400

    Added pseudocode for Stripe's  event

--- backend/controllers/webhookController.ts
+++ backend/controllers/webhookController.ts
@@ -17,17 +17,23 @@ export function handle(req: Request, res: Response) {
     /*
     let event = constructEvent(req.body);
 
-    createMigration(Event);
-
     switch (event.type) {
         // Because `dripos-migration-database` stores data permanetly, we do 
         // not need to respond to deletion events other than to demarcate that
         // said event deletes some data (e.g: source deletion events)
         case 'customer.created': {
-        
+
         },
         case 'customer.updated': {
-        
+            // Check and get customer if its id is in our Redis cache, if not 
+            // use the event's data
+            let customer = await getCache(data.id);
+            if (!customer) {
+                customer = event.data.object;
+            }
+
+            let status = await updateCustomer(customer);
+            await createMigration(status, event);
         },
         case 'customer.source.created': {
         
